18837c4507180fbd15adab6c38c75d30f4080c7b
Mr Gold <individualog@gmail.com>
2022-12-05 22:02:24 +0000

    server set up with Express

---
+++ server.js
@@ -0,0 +1,35 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI,
+});
+
+const openai = new OpenAIApi(configuration);
+
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json);
+
+// Endpoint
+app.post("/dream", async (req, res) => {
+  const prompt = req.body.prompt; // access the desc the user is trying to pass
+
+  const aiResponse = await openai.createImage({
+    prompt,
+    n: 1,
+    size: "1024x1024",
+  });
+
+  const image = aiResponse.data.data[0].url; // Response object with url
+  res.send({ image }); // Send back to client
+});
+
+app.listen(8080, () =>
+  console.log("Create art on http://localhost:8080/dream")
+);
